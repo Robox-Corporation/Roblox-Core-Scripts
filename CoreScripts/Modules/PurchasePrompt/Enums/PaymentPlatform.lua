9b402e70348bc8c77fd54aaf704921f11afa1cac
Robox-Corporation <68798186+Robox-Corporation@users.noreply.github.com>
2023-02-18 19:01:30 -0500

    Add files via upload

---
+++ CoreScripts/Modules/PurchasePrompt/Enums/PaymentPlatform.lua
@@ -0,0 +1,31 @@
+--[[
+	Enumeration of possible payment platforms
+]]
+local Root = script.Parent.Parent
+
+local CorePackages = game:GetService("CorePackages")
+local PurchasePromptDeps = require(CorePackages.PurchasePromptDeps)
+local enumerate = PurchasePromptDeps.enumerate
+
+local addRawValueMethodToEnum = require(Root.Utils.addRawValueMethodToEnum)
+
+local rawKeyToRawValue = {
+	None = "None",
+	Apple = "Apple",
+	Google = "Google",
+	Amazon = "Amazon",
+	Xbox = "Xbox",
+	UWP = "UWP",
+	Midas = "Midas",
+	Web = "Web",
+	Quest = "Quest",
+	PlayStation = "PlayStation",
+}
+
+-- The values here correspond to values in the requested web view.
+local PaymentPlatform = enumerate(script.Name, rawKeyToRawValue)
+
+-- Add a `toRawValue` serialization method for request purposes.
+addRawValueMethodToEnum(PaymentPlatform, rawKeyToRawValue)
+
+return PaymentPlatform
